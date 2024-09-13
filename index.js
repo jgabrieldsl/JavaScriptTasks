a63781a21d4978914eac189cfb7954390474f198
jgabsx <joao.gsl@puccampinas.edu.br>
2024-09-13 16:14:44 -0300

    Metas - Realizadas/Abertas/Deletar

--- index.js
+++ index.js
@@ -21,24 +21,26 @@ const cadastrarMeta = async () => {
     })
 
     console.log(metas)
-
 }
 
+// Arrow Function para listar metas
 const listarMetas = async () => {
     const respostas = await checkbox ({
         message: 'Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa.',
-        choices: [...metas]
+        choices: [...metas],
+        instructions: false
     })
-
-    if (respostas.length == 0) {
-        console.log("Nenhuma meta selecionada.")
-        return listarMetas()
-    }
 
     // Desmarcando todas as metas
     metas.forEach((m) => {
         m.checked = false
     })
+
+    // Se nenhuma meta for selecionada, aparece a seguinte mensagem.
+    if (respostas.length == 0) {
+        console.log("Nenhuma meta selecionada.")
+        return listarMetas()
+    }    
 
     // For para procurar no Array se a meta marcado é igual, se sim, marca a meta como concluída.
     respostas.forEach ((resposta) => {
@@ -49,8 +51,78 @@ const listarMetas = async () => {
     })
 
     console.log('Meta(s) concluídas!')
-
 }
+
+// Metas realizadas
+const metasRealizadas = async () => {
+    const realizadas = metas.filter ((meta) => {  /* HOF -> Sempre recebe uma função*/ 
+        return meta.checked
+    })
+
+    console.log(realizadas);
+
+    if (realizadas.length == 0) {
+        console.log("Nenhuma meta realizada")
+        return
+    }
+    
+    await select ({
+        message: "Metas realizadas: " + realizadas.length,
+        choices: [...realizadas],
+        instructions: false
+    })
+}
+
+// Metas em aberto
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => { /* HOF -> Sempre recebe uma função*/ 
+        return !meta.checked
+    })
+
+    if (abertas.length == 0) {
+        console.log("Nenhuma meta em aberto")
+        return
+    }
+
+    await select ({
+        message: "Metas em aberto: " + abertas.length,
+        choices: [...abertas],
+        instructions: false
+    })
+}
+
+// Arrow Function para deletar metas
+const deletarMetas = async () => {
+
+    if (metas.length == 0 ) {
+        console.log("Nenhuma meta cadastrada.")
+        return
+    }
+
+    const metasDesmarcadas = metas.map((meta) => {
+        return { value: meta.value, checked: false }
+    })
+    
+    const aDeletar = await checkbox ({
+        message: 'Selecione a meta a deletar',
+        choices: [...metasDesmarcadas],
+        instructions: false
+    })
+
+    if (aDeletar.length == 0) {
+        console.log("Nenhum item para deletar!")
+        return
+    }
+
+    aDeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item /* Só ira ficar na lista de metas aquilo que não for marcado */
+        })
+    })
+
+    console.log("Meta(s) deletada(s) com sucesso!")
+}
+
 
 // Menu principal - Função assíncrona
 const start = async () => {
@@ -68,11 +140,23 @@ const start = async () => {
                     value: 'Listar'
                 },
                 {
+                    name: 'Metas realizadas',
+                    value: 'Realizadas'
+                },
+                {
+                    name: 'Metas em aberto',
+                    value: 'Abertas'
+                },
+                {
+                    name: 'Deletar metas',
+                    value: 'Deletar'
+                },
+                {
                     name: 'Sair',
                     value: 'Sair'
                 }
-            ]
-
+            ],
+            instructions: false
         })
 
         switch (opcao) {
@@ -81,6 +165,15 @@ const start = async () => {
                 break
             case 'Listar':
                 await listarMetas()
+                break
+            case 'Realizadas':
+                await metasRealizadas()
+                break
+            case 'Abertas':
+                await metasAbertas()
+                break
+            case 'Deletar':
+                await deletarMetas()
                 break
             case 'Sair':
                 console.log('Até logo!')
